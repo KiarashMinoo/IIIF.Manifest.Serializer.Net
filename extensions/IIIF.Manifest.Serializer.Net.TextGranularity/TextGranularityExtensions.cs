758bf84a109d9dd7159600e3feb940cd54dfc4d2
Kiarash Minoo <yanis_1984@msn.com>
2026-02-25 10:51:18 +0330

    Refactor TextGranularityExtensions: Introduce extension methods for Image to set and retrieve TextGranularity

--- extensions/IIIF.Manifest.Serializer.Net.TextGranularity/TextGranularityExtensions.cs
+++ extensions/IIIF.Manifest.Serializer.Net.TextGranularity/TextGranularityExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using IIIF.Manifests.Serializer.Helpers;
+using IIIF.Manifests.Serializer.Nodes.Contents.Image;
 using IIIF.Manifests.Serializer.Shared.Content.Resources;
 using IIIF.Manifests.Serializer.Shared.Trackable;
 
@@ -6,6 +7,16 @@ namespace IIIF.Manifests.Serializer.Extensions;
 
 public static class TextGranularityExtensions
 {
+    extension(Image image)
+    {
+        public Image SetTextGranularity(TextGranularity textGranularity)
+        {
+            return image;
+        }
+
+        public TextGranularity? TextGranularity => image.GetAdditionalProperty<Image, TextGranularity>(TextGranularity.TextGranularityJName);
+    }
+
     extension<TResource>(TResource resource) where TResource : IBaseResource, IAdditionalPropertiesSupport<TResource>
     {
         public TResource SetTextGranularity(TextGranularity textGranularity)
